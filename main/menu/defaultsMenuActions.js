7a60581cc503c48560516794b82a095f4af3bfa9
Face Kapow <facekapow@outlook.com>
2016-08-06 12:15:38 -0400

    Bug fix

--- main/menu/defaultsMenuActions.js
+++ main/menu/defaultsMenuActions.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { app } = require('electron');
+
 exports.onToggleFullScreen = (item, window) => window.setFullScreen(!window.isFullScreen());
 
 exports.onQuit = (item, window) => app.quit();
